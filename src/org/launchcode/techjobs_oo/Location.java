9fcc69587352338bae4383c552f1e4ee1e8985ae
Rayna Schexnayder <Rayna.schexnayder@gmail.com>
2019-11-21 16:14:46 -0600

    Completed step 1 and partial step 2

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -15,6 +15,11 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
+    public void assignValue (String newValue){
+        new Location();
+        value = newValue;
+    }
+
 
     // Custom toString, equals, and hashCode methods:
 
