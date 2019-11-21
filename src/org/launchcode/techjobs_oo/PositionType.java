71aa6aec82d6413a29793e49f4d59535db2d0fb9
Rayna Schexnayder <Rayna.schexnayder@gmail.com>
2019-11-21 16:16:46 -0600

    completed step 2

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,9 +1,24 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.Objects;
+
 public class PositionType {
     private int id;
     private static int nextId = 1;
     private String value;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     public PositionType() {
         id = nextId;
@@ -33,4 +48,10 @@ public class PositionType {
     public void setValue(String value) {
         this.value = value;
     }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
 }
